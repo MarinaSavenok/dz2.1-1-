a6b0077607219c708153c97512dcbc85a8e0322b
Marina Savenok <Savenok-marina@mail.ru>
2022-01-12 12:13:52 +0300

    miles

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 25_010;
+        int bonus = 20;
+        int miles = amount / bonus;
+
+        System.out.println("Бонусные мили: " + miles);
+    }
+
+    int result = 1250;
+}
